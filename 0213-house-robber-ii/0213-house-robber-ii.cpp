7b25e59e111e3efbcec0dbfabacf8457e7cf9b56
Sankalp Suyash <sankalpsuyash98@gmail.com>
2025-10-06 10:20:51 +0530

    Time: 0 ms (100%), Space: 10.8 MB (40.81%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums, int i, int n) {
-        if (i > n) {
-            return 0;
-        }
-
-        if (t[i] != -1) {
-            return t[i];
-        }
-
-        int steal = nums[i] + solve(nums, i + 2, n);
-        int skip = solve(nums, i + 1, n);
-
-        return t[i] = max(steal, skip);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
+
         if (n == 1) {
             return nums[0];
         }
 
-        if (n == 2) {
-            return max(nums[0], nums[1]);
+        vector<int> t(n + 1, 0);
+
+        t[0] = 0;
+
+        for (int i = 1; i <= n - 1; i++) {
+            int stole = nums[i - 1] + ((i - 2 >= 0) ? t[i - 2] : 0);
+            int skip = t[i - 1];
+            t[i] = max(stole, skip);
         }
 
-        memset(t, -1, sizeof(t));
-        int take_0th_index = solve(nums, 0, n - 2);
+        int result1 = t[n - 1];
+        t.clear();
 
-        memset(t, -1, sizeof(t));
-        int take_1st_index = solve(nums, 1, n - 1);
+        t[0] = 0;
+        t[1] = 0;
 
-        return max(take_0th_index, take_1st_index);
+        for (int i = 2; i <= n; i++) {
+            int stole = nums[i - 1] + ((i - 2 >= 0) ? t[i - 2] : 0);
+            int skip = t[i - 1];
+            t[i] = max(stole, skip);
+        }
+
+        int result2 = t[n];
+
+        return max(result1, result2);
     }
 };
