f5c76cad507c4a6e24ac952cb7cf9cf7f44cad92
Sankalp Suyash <sankalpsuyash98@gmail.com>
2026-03-03 08:24:11 +0530

    Time: 0 ms (100%), Space: 19 MB (47.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,9 +16,26 @@ public:
         if (root == NULL) {
             return 0;
         }
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
 
-        return 1 + max(left, right);
+        while (!q.empty()) {
+            int size = q.size();
+            depth++;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if (curr->left) {
+                    q.push(curr->left);
+                }
+                if (curr->right) {
+                    q.push(curr->right);
+                }
+            }
+        }
+
+        return depth;
     }
 };
