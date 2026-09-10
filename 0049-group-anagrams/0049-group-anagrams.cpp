91064978fc07786cbb16ffc68b4e3d63e3616f3f
Sankalp Suyash <sankalpsuyash98@gmail.com>
2026-09-10 18:11:33 +0530

    Time: 16 ms (59.15%), Space: 26.1 MB (53.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
-    bool isAnagram(const string& s, const string& t) {
-        if (s.length() != t.length())
-            return false;
-
-        int freq[26] = {0};
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
-        }
-
-        for (int count : freq) {
-            if (count != 0)
-                return false;
-        }
-
-        return true;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;
         int n = strs.size();
-        vector<bool> visited(n, false);
+
+        vector<vector<string>> result;
+
+        unordered_map<string, vector<string>> mp;
 
         for (int i = 0; i < n; i++) {
-            if (visited[i])
-                continue;
+            string temp = strs[i];
 
-            vector<string> group;
-            group.push_back(strs[i]);
-            visited[i] = true;
+            sort(temp.begin(), temp.end());
 
-            for (int j = i + 1; j < n; j++) {
-                if (!visited[j] && isAnagram(strs[i], strs[j])) {
-                    group.push_back(strs[j]);
-                    visited[j] = true;
-                }
-            }
+            mp[temp].push_back(strs[i]);
+        }
 
-            result.push_back(group);
+        for (auto it : mp) {
+            result.push_back(it.second);
         }
 
         return result;
