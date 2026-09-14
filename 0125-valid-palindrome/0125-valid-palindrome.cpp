80af72dbc21c4692462152522a14491088038a58
Sankalp Suyash <sankalpsuyash98@gmail.com>
2026-09-14 11:03:21 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.57%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int low = 0;
+        int high = s.length() - 1;
+
+        while (low < high) {
+            while (low < high && !isalnum(s[low])) {
+                low++;
+            }
+            while (low < high && !isalnum(s[high])) {
+                high--;
+            }
+
+            if (tolower(s[low]) != tolower(s[high])) {
+                return false;
+            }
+
+            low++;
+            high--;
+        }
+
+        return true;
+    }
+};
