b77bed44a8dec9e4f725fea240dc04b05494bd54
Sankalp Suyash <sankalpsuyash98@gmail.com>
2026-09-14 11:27:09 +0530

    Time: 2 ms (10.68%), Space: 25.7 MB (5.21%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int, int> mp;
-        for (int i = 0; i < numbers.size(); i++) {
-            int tmp = target - numbers[i];
-            if (mp.count(tmp)) {
-                return {mp[tmp], i + 1};
+        int l = 0;
+        int r = numbers.size() - 1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum > target) {
+                r--;
             }
-            mp[numbers[i]] = i + 1;
+            else if (sum < target) {
+                l++;
+            }
+            else {
+                return {l + 1, r + 1};
+            }
         }
+
         return {};
     }
 };
