226f7c9c60a191fc8d1f7d152c37e0c6d3766dec
Sankalp Suyash <sankalpsuyash98@gmail.com>
2026-09-19 10:27:43 +0530

    Time: 0 ms (100%), Space: 12.6 MB (25.94%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -6,23 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        // dfs approach
-
-        if(!root) {
+        // DFS - iterative approach using stack.
+        if (!root) {
             return nullptr;
         }
 
-        swap(root -> left, root -> right);
-        
-        invertTree(root -> left);
-        invertTree(root -> right);
+        stack<TreeNode*> stack;
+        stack.push(root);
 
+        while (!stack.empty()) {
+            TreeNode* node = stack.top();
+            stack.pop();
+
+            swap(node->left, node->right);
+
+            if (node->left) {
+                stack.push(node->left);
+            }
+
+            if (node->right) {
+                stack.push(node->right);
+            }
+        }
         return root;
     }
 };
